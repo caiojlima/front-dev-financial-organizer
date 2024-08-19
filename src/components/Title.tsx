3d891627a48893f8c6b3cb7bfaae0df445be0e88
Caio Lima <caiojlimah@gmail.com>
2024-08-18 23:55:47 -0300

    [FIX]: Title Style

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,9 +2,9 @@ import { Box } from "@mui/material";
 import logo from "../assets/Logo.svg";
 
 const Title = () => (
-  <Box sx={{ width: "60%  ", display: "flex", justifyContent: "space-around", alignItems: 'center', fontSize: "2vw" }}>
+  <Box sx={{ width: "60%  ", display: "flex", alignItems: 'center', fontSize: "2vw" }}>
     <img style={{ width: "20vw" }} src={ logo } alt="Logo" />
-    <h1 style={ { color: "rgba(45, 146, 42, 0.37)" } }>Financial Organizer</h1>
+    <h1 style={ { color: "rgba(45, 146, 42, 0.37)", textAlign: "center" } }>Financial Organizer</h1>
   </Box>
 );
 
